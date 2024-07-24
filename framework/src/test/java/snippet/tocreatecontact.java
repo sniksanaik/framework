18d2c7e6a29b6035db2bf7ad2c32474f0a547bc2
sniksanaik <sniksa10@gmail.com>
2024-07-24 08:53:09 +0530

    new push

--- framework/src/test/java/snippet/tocreatecontact.java
+++ framework/src/test/java/snippet/tocreatecontact.java
@@ -2,7 +2,16 @@ package snippet;
 
 public class tocreatecontact {
 	public static void main(String[] args) {
+		System.out.println("hi");
+		System.out.println("change done");
 		
+		
+		
+		System.out.println("new change");
+		System.out.println("new change");
+		System.out.println("new change");
+		System.out.println("new change");
+		System.out.println("new change");
 	}
 }
 
